092627a338523ffea3d981dcb9381ab22c5eed28
Amanda Perch <perch_amanda@hotmail.com>
2022-02-03 14:39:43 -0300

    Adiciona operaçoes

---
+++ fundamentos/bloco_04_introdução_a_JavaScript_e_logica_de_programação/JavaScript_Primeiros_Passos/Exercicio1_Logica_Condicional.js
@@ -0,0 +1,11 @@
+const valorA = 3  
+const valorB = 6
+
+console.log (valorA + valorB)
+console.log (valorA - valorB)
+console.log (valorA * valorB)
+console.log (valorA / valorB)
+console.log (valorA % valorB)
+
+
+

706a0994ca1922c6b5b099a692efddc7245eb525
Amanda Perch <perch_amanda@hotmail.com>
2022-02-03 19:06:04 -0300

    Exercicios 2/3/4/5

--- fundamentos/bloco_04_introdução_a_JavaScript_e_logica_de_programação/JavaScript_Primeiros_Passos/Exercicio1_Logica_Condicional.js
+++ fundamentos/bloco_04_introdução_a_JavaScript_e_logica_de_programação/JavaScript_Primeiros_Passos/Exercicio1_Logica_Condicional.js
@@ -7,5 +7,46 @@ console.log (valorA * valorB)
 console.log (valorA / valorB)
 console.log (valorA % valorB)
 
+const num1 = 7
+const num2 = 65
+if (num1 > num2){
+  console.log ("'a' é maior que 'b' ")}
+else {
+ console.log ("'b' é maior que 'a'")
+}
+
+const number1 = 9
+const number2 = 3
+const number3 =12
+if (number1 > number2 && number1 > number3){
+  console.log("o maior numero é number1")
+}
+else if (number2 > number1 && number2 > number3){
+  console.log ("o maior é o numero 2")
+}
+else {
+  console.log ("o maior é o numero 3")
+}
+
+const numero1 = 45
+if (numero1 > 0){
+  console.log ("positive")
+}
+  else if (numero1 < 0){
+    console.log ("negative")
+  }
+  else {
+    console.log("zero")
+  }
+
+const angulo1 = 60
+const angulo2 = 60
+const angulo3 = 60
+if (angulo1 + angulo2 + angulo3 === 180){
+  console.log("true")
+  }
+else {
+  console.log("false")
+}
 
 

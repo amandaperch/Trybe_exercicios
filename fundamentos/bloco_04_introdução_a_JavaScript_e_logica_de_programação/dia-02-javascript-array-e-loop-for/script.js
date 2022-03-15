8e570973b6106c0c3fa93436d66dcefb0e68b333
Amanda Perch <perch_amanda@hotmail.com>
2022-03-15 19:29:19 -0300

    Adiciona arquivo e questao 1

---
+++ fundamentos/bloco_04_introdução_a_JavaScript_e_logica_de_programação/dia-02-javascript-array-e-loop-for/script.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;// 
+console.log(numbers[0])
+console.log(numbers[1])
+console.log(numbers[2])
+console.log(numbers[3])
+console.log(numbers[4])
+console.log(numbers[5])
+console.log(numbers[6])
+console.log(numbers[7])
+console.log(numbers[8])
+console.log(numbers[9])
+//Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;//

1fa054c12c5520b94c5d636bb383e3306e963e55
Amanda Perch <perch_amanda@hotmail.com>
2022-04-12 15:45:13 -0300

    Adiciona arquivos

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/aula10.1-app/src/textJSX.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class TextJSX extends React.Component{
+  rende(){
+    return(
+      <div>
+        <h1>
+          Hello, JSX
+        </h1>
+      </div>
+    )
+  }
+
+}
+
+export default TextJSX;

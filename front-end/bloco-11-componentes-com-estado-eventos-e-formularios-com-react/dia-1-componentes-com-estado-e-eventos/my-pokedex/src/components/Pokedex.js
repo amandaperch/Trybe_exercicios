c60bb2bfd7d27ebd6a408306004c580ab06f130a
Amanda Perch <perch_amanda@hotmail.com>
2022-04-16 17:45:31 -0300

    Adiciona botoes e ações

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/my-pokedex/src/components/Pokedex.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/my-pokedex/src/components/Pokedex.js
@@ -1,18 +1,70 @@
 import React from "react";
 import Pokemon from "./Pokemon";
 
-const renderPokemom = (pokemon) => (
-  <Pokemon key={pokemon.id} pokemon={pokemon} />
-)
-
 class Pokedex extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      index: 0,
+      type: 'Fire'
+    };
+    this.nextPokemon = this.nextPokemon.bind(this);
+  };
+
+  filterPokemon = ({target}) =>{
+    this.setState({ type: target.innerText })
+  };
+
+  byType = (pokemon) =>{
+    const { type } = this.state;
+    return pokemon.type === type;
+  };
+
+  nextPokemon(){
+    const { pokemons } = this.props;
+    const pokemonsFiltered = pokemons.filter(this.byType);
+    this.setState((estadoAnterior, _props) => {
+      if (estadoAnterior.index === pokemonsFiltered.length - 1){
+        return { index: 0 }
+      }
+      return{
+        index: estadoAnterior.index + 1,
+      }
+    });
+  }
+
   render(){
     const { pokemons } = this.props;
+    const { index } = this.state;
     return(
       <div>
-        {
-        pokemons.map(renderPokemom)
-        }
+        {<Pokemon pokemon={pokemons.filter(this.byType)[index]} />}
+        <button onClick={this.nextPokemon}>
+          Próximo Pokemon
+        </button>
+        <br/>
+        <button onClick={this.filterPokemon}>
+          Fire
+        </button>
+        <button onClick={this.filterPokemon}>
+          Psychic
+        </button>
+        <button onClick={this.filterPokemon}>
+          Electric
+        </button>
+        <button onClick={this.filterPokemon}>
+          Bug
+        </button>
+        <button onClick={this.filterPokemon}>
+          Poison
+        </button>
+        <button onClick={this.filterPokemon}>
+          Normal
+        </button>
+        <button onClick={this.filterPokemon}>
+          Dragon
+        </button>        
       </div>
     );
   }
